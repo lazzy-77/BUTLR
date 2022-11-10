fa3bd3096d26c4c7d0fd19b792e8fc6452083200
40266751 <hnieva01@qub.ac.uk>
2022-11-10 03:46:23 +0000

    fixed app js

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { store } from "./app/redux/store";
+import { Provider } from "react-redux";
+import AppNavigator from './app/navigation/AppNavigator'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <AppNavigator />
+        </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
