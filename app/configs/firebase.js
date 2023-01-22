f69474e26851056f698cb480d54675e7c99aa2b3
40266751 <hnieva01@qub.ac.uk>
2023-01-22 04:52:01 +0000

    refactored firebase config

--- app/configs/firebase.js
+++ app/configs/firebase.js
@@ -1,11 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import firebase from "firebase/compat";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { serverTimestamp } from "firebase/firestore";
+import { getFunctions, httpsCallable } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-require('firebase/auth')
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,10 +21,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
-const auth = app.auth()
-const db = firebase.firestore()
-const timestamp = firebase.firestore.FieldValue.serverTimestamp()
-// const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const timestamp = serverTimestamp();
+const functions = getFunctions(app);
 
-export {auth, db, timestamp}
+export {auth, db, timestamp, functions, httpsCallable}
