f99cb570f7a0ae611536a24043f0d307d6e046ba
40266751 <hnieva01@qub.ac.uk>
2023-02-01 03:21:27 +0000

    updated categories list

--- app/data/categoriesData.js
+++ app/data/categoriesData.js
@@ -1,21 +1,66 @@
-
 export const categoriesList = [
-    'cleaning-and-laundry',
-    'shopping-and-errands',
-    'auto-and-vehicles',
-    'home-improvement-and-handyman',
-    'gardening-and-lawn-care',
-    'food-and-catering',
-    'personal-styling-and-fashion',
-    'beauty-and-spa',
-    'childcare',
-    'pet-care',
-    'music-and-entertainment',
-    'photography-and-videography',
-    'disposal-and-garbage',
-    'moving-and-storage',
-    'other',
+    {
+        category: 'cleaning-and-laundry',
+        displayName: 'Cleaning and Laundry',
+    },
+    {
+        category: 'shopping-and-errands',
+        displayName: 'Shopping and Errands',
+    },
+    {
+        category: 'auto-and-vehicles',
+        displayName: 'Auto and Vehicles',
+    },
+    {
+        category: 'home-improvement-and-handyman',
+        displayName: 'Home Improvement and Handyman',
+    },
+    {
+        category: 'gardening-and-lawn-care',
+        displayName: 'Gardening and Lawn Care',
+    },
+    {
+        category: 'food-and-catering',
+        displayName: 'Food and Catering',
+    },
+    {
+        category: 'personal-styling-and-fashion',
+        displayName: 'Personal Styling and Fashion',
+    },
+    {
+        category: 'beauty-and-spa',
+        displayName: 'Beauty and Spa',
+    },
+    {
+        category: 'childcare',
+        displayName: 'Childcare',
+    },
+    {
+        category: 'pet-care',
+        displayName: 'Pet Care',
+    },
+    {
+        category: 'music-and-entertainment',
+        displayName: 'Music and Entertainment',
+    },
+    {
+        category: 'photography-and-videography',
+        displayName: 'Photography and Videography',
+    },
+    {
+        category: 'disposal-and-garbage',
+        displayName: 'Disposal and Garbage',
+    },
+    {
+        category: 'moving-and-storage',
+        displayName: 'Moving and Storage',
+    },
+    {
+        category: 'other',
+        displayName: 'Other',
+    },
 ];
+
 export const categoriesData = [
     {
         image: require("../assets/images/iron.png"),
